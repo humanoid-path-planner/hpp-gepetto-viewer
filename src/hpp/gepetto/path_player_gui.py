f02fe2b65f4e5bc8932f28fc7e399fd58889ae2a
Joseph Mirabel <jmirabel@laas.fr>
2015-01-22 17:03:10 +0100

    Do not use multithreading.
      * This works fine as long as the following two operations are fast
        (wrt to the human perception)
        ** get a configuration from hpp server
        ** publish a configuration to viewer server

--- src/hpp/gepetto/path_player_gui.py
+++ src/hpp/gepetto/path_player_gui.py
@@ -15,7 +15,6 @@
 # hpp-gepetto-viewer.  If not, see
 # <http://www.gnu.org/licenses/>.
 
-from multiprocessing import Event, Lock, Process
 import sys, pygtk, time, gtk, glib
 pygtk.require("2.0")
 
@@ -30,8 +29,8 @@ class PathPlayerGui:
     self.client = client
     self.t = 0;
     self.dt = 0.01
-    self.isPlaying  = Lock ()
-    self.pauseRequest = Event ()
+    self.isPlaying  = False
+    self.pauseRequest = False
     self.pathplayer = None
     handlers = {
       "on_Speed_value_changed": self.on_speed_changed,
@@ -63,16 +62,12 @@ class PathPlayerGui:
     self.glade.get_object ("PathScale").set_range (0, self.pathLength)
 
   def on_play_clicked (self, w):
-    if self.isPlaying.acquire (False):
-      self.pathplayer = Process (target = self.playpath)
-      self.pathplayer.start ()
+    if not self.isPlaying:
+      glib.timeout_add (int (1000*self.dt), self.path_pulse)
 
   def on_pause_clicked (self, w):
-    if self.pathplayer is not None and self.pathplayer.is_alive ():
-      self.pauseRequest.set ()
-      self.pathplayer.join ()
-      self.pauseRequest.clear ()
-    self.pathplayer = None
+    if self.isPlaying:
+      self.pauseRequest = True
 
   def on_pathscale_changed (self, w):
     if self.pathplayer is not None and self.pathplayer.is_alive ():
@@ -82,16 +77,16 @@ class PathPlayerGui:
     self.publisher.robotConfig = q
     self.publisher.publishRobots ()
 
-  def playpath (self):
-    while self.t < self.pathLength:
-      q = self.client.problem.configAtParam (self.pathId, self.t)
-      self.publisher.robotConfig = q
-      self.publisher.publishRobots ()
-      if self.pauseRequest.is_set ():
-        break
-      self.t += self.dt
-      time.sleep (self.dt)
-    self.isPlaying.release ()
+  def path_pulse (self):
+    if self.pauseRequest or self.t > self.pathLength:
+      if self.t > self.pathLength:
+        self.t = self.pathLength
+      self.pauseRequest = False
+      self.isPlaying = False
+      return False
+    self.t += self.dt
+    self.glade.get_object ("PathScale").set_value (self.t)
+    return True
 
   def show (self):
     self.glade.get_object ("MainWindow").show_all ()
@@ -100,4 +95,3 @@ class PathPlayerGui:
   def quit (self, window):
     self.glade.get_object ("PauseButton").clicked ()
     gtk.main_quit ()
-
