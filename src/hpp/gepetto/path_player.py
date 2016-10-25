b2d02de90e510932ba2623a145f07f258828b5bf
Pierre Fernbach <pierre.fernbach@laas.fr>
2016-10-25 15:29:00 +0200

    add method 'displayVelocityPath' in PathPlayer

--- src/hpp/gepetto/path_player.py
+++ src/hpp/gepetto/path_player.py
@@ -18,6 +18,7 @@
 import time
 import numpy as np
 import pickle as pk
+import math
 
 ## displays a path by sampling configurations along the path.
 #
@@ -68,6 +69,39 @@ class PathPlayer (object):
       nameCurve = "path_"+str(pathId)+"_"+jointName
       self.publisher.client.gui.addCurve(nameCurve,pathPos,color)
       self.publisher.client.gui.addToGroup(nameCurve,self.publisher.sceneName)
+      self.publisher.client.gui.refresh()
+
+    def displayVelocityPath(self,pathId,jointName=0) :
+      if jointName == 0 :
+        if self.publisher.robot.rootJointType == 'planar' :
+          jointName = self.publisher.robot.tf_root+'_joint'
+      vmax=self.client.problem.getParameter("vMax") * math.sqrt(3)
+      configSize = self.client.robot.getConfigSize() - self.client.robot.getDimensionExtraConfigSpace()
+      last_q = 0
+      length = self.end*self.client.problem.pathLength (pathId)
+      if jointName == 0 :
+        nameCurve = "path_"+str(pathId)+"_root"
+      else:
+        nameCurve = "path_"+str(pathId)+"_"+jointName
+      self.publisher.client.gui.createGroup(nameCurve)
+      self.publisher.client.gui.addToGroup(nameCurve,self.publisher.sceneName)
+      t = self.start*self.client.problem.pathLength (pathId)
+      while t < length :
+        q = self.client.problem.configAtParam (pathId, t)
+        if jointName != 0 :
+          self.publisher.robot.setCurrentConfig(q)
+          q = self.publisher.robot.getLinkPosition(jointName)
+        if last_q != 0 :
+          name = nameCurve+"_line_"+str(t)
+          v = (math.sqrt(q[configSize] * q[configSize] + q[configSize+1] * q[configSize+1] + q[configSize+2] * q[configSize+2]))/vmax
+          if v<0.5:
+            color=[v*2.,1,0,1]
+          else:
+            color=[1,2-(v*2.),0,1]
+          self.publisher.client.gui.addLine(name,last_q,q[0:3],color)
+          self.publisher.client.gui.addToGroup(name,nameCurve)
+        last_q = q[0:3]
+        t += self.dt
       self.publisher.client.gui.refresh()
 
 
