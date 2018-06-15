e51b9761ded14b5df4eef3de17b5dc59ab7967a5
Joseph Mirabel <jmirabel@laas.fr>
2018-06-15 10:40:56 +0200

    Update doc

--- doc/main.hh
+++ doc/main.hh
@@ -10,10 +10,10 @@
 ///     hppcorbaserver and gepetto-viewer-server,
 /// \li gepetto.manipulation.viewer.Viewer that implements a simultaneous
 ///     control to hpp-manipulation-server and gepetto-viewer-server,
-/// \li gepetto.delayed_viewer.DelayedViewer that implements a wrapper
+/// \li gepetto.viewer_factory.ViewerFactory that implements a wrapper
 ///     that stores commands to be sent to gepetto-viewer-server and
 ///     creates new clients when gepetto-viewer-server is restarted,
-/// \li gepetto.manipulation.delayed_viewer.DelayedViewer that
-///     implements a delayed viewer for hpp-manipulation-server,
+/// \li gepetto.manipulation.viewer_factory.ViewerFactory that
+///     implements a viewer factory for hpp-manipulation-server,
 /// \li gepetto.path_player.PathPlayer that displays a path by sampling
 ///     configurations along the path.
