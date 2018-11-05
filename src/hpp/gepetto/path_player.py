e6e4cfb957cfdfc65ef8f90dc254ee1cf2f8d5a7
pfernbac <pierre.fernbach@gmail.com>
2018-11-05 15:27:06 +0100

    add method pathPlayer.displayVelocityPath

--- src/hpp/gepetto/path_player.py
+++ src/hpp/gepetto/path_player.py
@@ -18,6 +18,8 @@
 import time
 import numpy as np
 import pickle as pk
+import math
+import omniORB.any
 
 import hpp.corbaserver.client
 
@@ -53,6 +55,8 @@ class PathPlayer (object):
             if elapsed < self.dt :
               time.sleep(self.dt-elapsed)
 
+    ## Display the path of the desired joint
+    # By default, display the path of the root
     def displayPath(self,pathId,color=[0.85,0.75,0.15,0.9],jointName=0) :
       if jointName == 0 :
         if self.publisher.robot.rootJointType == 'planar' :
@@ -72,6 +76,42 @@ class PathPlayer (object):
       nameCurve = "path_"+str(pathId)+"_"+jointName
       self.publisher.client.gui.addCurve(nameCurve,pathPos,color)
       self.publisher.client.gui.addToGroup(nameCurve,self.publisher.sceneName)
+      self.publisher.client.gui.refresh()
+
+    ## Display the path with a color variation according to the velocity along the path
+    # green for velocity close to 0, red for velocity close to the velocity bound.
+    # This method assume that the path is represented by instances of the KinodynamicPath class
+    def displayVelocityPath(self,pathId,jointName=0) :
+      if jointName == 0 :
+        if self.publisher.robot.rootJointType == 'planar' :
+          jointName = self.publisher.robot.tf_root+'_joint'
+      vmax=omniORB.any.from_any(self.client.problem.getParameter("Kinodynamic/velocityBound")) * math.sqrt(2)
+      configSize = self.client.robot.getConfigSize() - self.client.robot.getDimensionExtraConfigSpace()
+      last_q = 0
+      length = self.end*self.client.problem.pathLength (pathId)
+      if jointName == 0 :
+        nameCurve = "path_"+str(pathId)+"_root"
+      else:
+        nameCurve = "path_"+str(pathId)+"_"+jointName
+      self.publisher.client.gui.createGroup(nameCurve)
+      self.publisher.client.gui.addToGroup(nameCurve,self.publisher.sceneName)
+      t = self.start*self.client.problem.pathLength (pathId)
+      while t < length :
+        q = self.client.problem.configAtParam (pathId, t)
+        if jointName != 0 :
+          self.publisher.robot.setCurrentConfig(q)
+          q = self.publisher.robot.getLinkPosition(jointName)
+        if last_q != 0 :
+          name = nameCurve+"_line_"+str(t)
+          v = (math.sqrt(q[configSize] * q[configSize] + q[configSize+1] * q[configSize+1] + q[configSize+2] * q[configSize+2]))/vmax
+          if v<0.5:
+            color=[v*2.,1,0,1]
+          else:
+            color=[1,2-(v*2.),0,1]
+          self.publisher.client.gui.addLine(name,last_q,q[0:3],color)
+          self.publisher.client.gui.addToGroup(name,nameCurve)
+        last_q = q[0:3]
+        t += self.dt
       self.publisher.client.gui.refresh()
 
     ## Generate a plot with the following datas:
