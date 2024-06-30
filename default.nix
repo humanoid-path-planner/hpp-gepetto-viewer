405c8d4292fe1639a6ed01ab4adb55e8262a3820
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-06-30 11:04:36 +0200

    nix: package

---
+++ default.nix
@@ -0,0 +1,49 @@
+{
+  lib,
+  cmake,
+  gepetto-viewer-corba,
+  hpp-corbaserver,
+  libsForQt5,
+  pkg-config,
+  python3Packages,
+}:
+
+python3Packages.buildPythonPackage {
+  pname = "hpp-gepetto-viewer";
+  version = "5.0.0";
+  pyproject = false;
+
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.unions [
+      ./CMakeLists.txt
+      ./doc
+      ./package.xml
+      ./src
+    ];
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    cmake
+    libsForQt5.wrapQtAppsHook
+    pkg-config
+  ];
+  buildInputs = [
+    libsForQt5.qtbase
+  ];
+  propagatedBuildInputs = [
+    gepetto-viewer-corba
+    hpp-corbaserver
+  ];
+
+  doCheck = true;
+
+  meta = {
+    description = "Display of hpp robots and obstacles in gepetto-viewer";
+    homepage = "https://github.com/humanoid-path-planner/hpp-gepetto-viewer";
+    license = lib.licenses.bsd2;
+    maintainers = [ lib.maintainers.nim65s ];
+  };
+}
