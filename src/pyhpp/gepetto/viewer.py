0609d8104402ee246adb19d45edc295d3eb2720f
Paul Sardin <paulsardin123@gmail.com>
2025-06-11 13:02:23 +0200

    add displayPath() to pyhpp viewer

--- src/pyhpp/gepetto/viewer.py
+++ src/pyhpp/gepetto/viewer.py
@@ -1,13 +1,23 @@
-import numpy
 from math import pi
+import numpy as np
 from gepetto.corbaserver import Client
 from gepetto import Error as GepettoError
-from pinocchio import forwardKinematics, updateGeometryPlacements
+from pinocchio import forwardKinematics, updateGeometryPlacements, SE3
 from eigenpy import Quaternion
-from pyhpp.manipulation import urdf
+from pyhpp.pinocchio import urdf
+from pyhpp.core import path
+
+import time
 
 class Viewer:
 
+    def displayPath(self, path):
+        for i in range(path.numberPaths()):  
+            for t in np.linspace(0,path.pathAtRank(i).length(),1000):
+                q, success = path.pathAtRank(i).eval(t)
+                if success:
+                    self.applyConfiguration(q)
+                    time.sleep(0.001)  
 
     def createWindowAndScene(self, name):
         self.windowName = "scene_hpp_" + name
@@ -16,6 +26,11 @@ class Viewer:
         except GepettoError:
             self.windowId = self.client.gui.createWindow(self.windowName)
         self.sceneName = self.windowName
+
+    def addURDFObstacleToScene(self, filename, prefix):
+        urdf.loadModel(self.robot, 0, prefix, "anchor", filename, "", SE3.Identity());
+        self.client.gui.addUrdfObjects(prefix, filename, True)
+        self.client.gui.addToGroup(prefix, self.sceneName)
 
     def addURDFToScene(self, frameIndex, prefix, rootType, urdfFilename, srdfFilename, pose):
         urdf.loadModel(self.robot, frameIndex, prefix, rootType, urdfFilename, srdfFilename, pose)
@@ -40,4 +55,3 @@ class Viewer:
             pose1 = list(pose.translation) + list(Quaternion(pose.rotation).coeffs())
             self.client.gui.applyConfiguration(o, pose1)
         self.client.gui.refresh()
-
